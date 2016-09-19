2d52281436b58ca8abdbf5759b91f8d3f626e347
Brett New and Stephanie Gurung <brett+stephanie@>
2016-09-19 16:29:17 -0700

    highlight selected option

--- app/assets/javascripts/questions.js
+++ app/assets/javascripts/questions.js
@@ -1,6 +1,7 @@
 jQuery.fn.submitOnClick = function() {
   this.find('input[type=submit]').click(function(){
     $(this).parent('form').submit();
+    $(this).parents('form').find('div').addClass('selected-option')
   });
   return this;
 }
