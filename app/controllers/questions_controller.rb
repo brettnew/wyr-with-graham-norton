535a363f13d799a777628584c5fdee6c2d6fc5fe
Brett New and Stephanie Gurung <brett+stephanie@>
2016-09-19 11:56:50 -0700

    allow user to vote for an option, update total votes

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,19 @@
 class QuestionsController < ApplicationController
   def index
     @questions = Question.all
+    @question = Question.new
+  end
+
+  def update
+    @question = Question.find(params[:id])
+    if @question.update(question_params)
+      redirect_to :back
+    end
   end
 
 
+private
+  def question_params
+    params.require(:question).permit(:option1, :option2, :option1_votes, :option2_votes)
+  end
 end
