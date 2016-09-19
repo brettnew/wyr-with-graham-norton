9226348ea2315e09220cf303a7823349dfa9919c
Brett New and Stephanie Gurung <brett+stephanie@>
2016-09-19 13:38:02 -0700

    display the percentage of votes for option 1 and option 2

--- app/models/question.rb
+++ app/models/question.rb
@@ -5,4 +5,10 @@ class Question < ActiveRecord::Base
   def totalVotes
     return self.option1_votes + self.option2_votes
   end
+
+  def average(votes)
+    if self.totalVotes > 0
+    return (votes.to_f/self.totalVotes.to_f * 100).round
+    end
+  end
 end
